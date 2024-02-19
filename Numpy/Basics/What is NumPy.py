bfe45bd70f3c1b9c478ee341a8382ddef5219560
George Smith <40671362+GLSmith155@users.noreply.github.com>
2024-02-19 16:59:35 -0500

    Update What is NumPy.py

--- Numpy/Basics/What is NumPy.py
+++ Numpy/Basics/What is NumPy.py
@@ -90,13 +90,15 @@ the buffer containing the actual elements of the array. Normally, we won’t nee
 
 Here are examples of ndarray's main attributes:
 '''
-a = np.arange(15).reshape(3, 5) ''' This is a clean way to go ahead and establish the dimensions of array a.
-                                    And if we remove this line, then all of the below code still works.
-                                    Do this when you want to go ahead and ensure that we don't mistype and
-                                    create an array of incorrect dimensions when we type in the numberes below. '''
+c = np.arange(15).reshape(3, 5) # Creates the same array that we manually create next called a.
+print(c)                        #          [[ 0  1  2  3  4]  
+                                #           [ 5  6  7  8  9]  
+                                #           [10 11 12 13 14]]
+
 a = np.array([[ 0,  1,  2,  3,  4],
               [ 5,  6,  7,  8,  9],
               [10, 11, 12, 13, 14]])
+
 print(a.shape)         #  (3, 5)
 
 print(a.ndim)          #  2
