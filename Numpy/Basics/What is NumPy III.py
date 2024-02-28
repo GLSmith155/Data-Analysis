5dbf5266ddb94ca35a4654ecbb495b1f197908fe
George Smith <40671362+GLSmith155@users.noreply.github.com>
2024-02-28 18:14:14 -0500

    Update What is NumPy III.py

--- Numpy/Basics/What is NumPy III.py
+++ Numpy/Basics/What is NumPy III.py
@@ -143,3 +143,34 @@ a = np.arange(int(1e8))
 b = a[:100].copy()
 del a  # the memory of ``a`` can be released.
 # If b = a[:100] is used instead, a is referenced by b and will persist in memory even if del a is executed.
+
+
+'''  
+                                                        Functions and methods overview
+Here is a list of some useful NumPy functions and methods names ordered in categories. See Routines and objects by topic for the full list.
+
+Array Creation:
+arange, array, copy, empty, empty_like, eye, fromfile, fromfunction, identity, linspace, logspace, mgrid, ogrid, ones, ones_like, r_, zeros, zeros_like
+
+Conversions:
+ndarray.astype, atleast_1d, atleast_2d, atleast_3d, mat
+
+Manipulations:
+array_split, column_stack, concatenate, diagonal, dsplit, dstack, hsplit, hstack, ndarray.item, newaxis, ravel, repeat, reshape, resize, squeeze, swapaxes, take, transpose, vsplit, vstack
+
+Questions:
+all, any, nonzero, where
+
+Ordering:
+argmax, argmin, argsort, max, min, ptp, searchsorted, sort
+
+Operations:
+choose, compress, cumprod, cumsum, inner, ndarray.fill, imag, prod, put, putmask, real, sum
+
+Basic Statistics:
+cov, mean, std, var
+
+Basic Linear Algebra:
+cross, dot, outer, linalg.svd, vdot
+
+''' 
